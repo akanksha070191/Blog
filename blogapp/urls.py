7781ffc305a7b5f7b25de2a8560c33f503513ef1
akanksha070191 <akanksha@grandortus.com>
2025-02-28 11:03:25 +0530

    blogDetailPage added

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -7,7 +7,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.blog, name='blog'),
-    path('signin', views.signin, name='signin')
+    path('signin', views.signin, name='signin'),
+    path('blogs/<int:blog_id>', views.blogDetail, name='blogDetail')
     
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- blogapp/views.py
+++ blogapp/views.py
@@ -7,3 +7,8 @@ def blog(request):
 
 def signin(request):
     return render(request, 'signin.html')
+
+def blogDetail(request, blog_id):
+    blog = BlogPost.objects.get(id=blog_id)
+    otherBlog = BlogPost.objects.exclude(id=blog_id).order_by('-created_on')
+    return render(request, 'blogDetail.html', {'blog':blog, 'otherBlog':otherBlog})
